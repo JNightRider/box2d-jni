713606ceb4b73f036e9b0971cd9f593bf5ec05d5
wil <nightrid34@gmail.com>
2026-05-17 16:45:54 -0600

    feat: b2Vec2 | struct

--- modules/libraries/core/src/main/java/org/box2d/jni/b2Vec2.java
+++ modules/libraries/core/src/main/java/org/box2d/jni/b2Vec2.java
@@ -48,84 +48,173 @@ public class b2Vec2 extends Struct<b2Vec2> {
         SIZEOF = layout.getSize();
         ALIGNOF = layout.getAlignment();
     }
-    
+
+    /**
+     * Generates a reference to the given pointer.
+     *
+     * @param ptr A reference pointer.
+     */
     public b2Vec2(Pointer ptr) {
         super(ptr);
     }
 
+    /**
+     * Create a new pointer to the object using its memory address.
+     *
+     * @param address A virtual memory address
+     */
     public b2Vec2(long address) {
         super(address);
     }
 
-    public b2Vec2(long address, boolean factor) {
+    /**
+     * This structure is for internal buffer use.
+     *
+     * @param address A virtual memory address
+     * @param factor boolean
+     *
+     * @see Struct#Struct(long, boolean)
+     */
+    protected b2Vec2(long address, boolean factor) {
         super(address, factor);
     }
 
+    /** @return Returns the property {@code x} */
     public float x() { return nx(address()); }
+    /** @return Returns the property {@code y} */
     public float y() { return ny(address()); }
     
+    /**
+     * Set the value of property {@code x}
+     *
+     * @param value The value
+     * @return b2Vec2
+     */
     public b2Vec2 x(float value) { nx(address(), value); return this; }
+    /**
+     * Set the value of property {@code y}
+     *
+     * @param value The value
+     * @return b2Vec2
+     */
     public b2Vec2 y(float value) { ny(address(), value); return this; }
     
+    /*(non-Javadoc)
+     */
     @Override
     public int sizeof() { return SIZEOF; }
-    
-    // -----------------------------------
-
+ 
+    /*(non-Javadoc)
+     */
     @Override
     protected b2Vec2 create(long address, Pointer ptr) {
         return ptr == null ? new b2Vec2(address) : new b2Vec2(ptr);
     }
     
+    // -----------------------------------
+
+    /**
+     * Internal use of the buffer.
+     * @return b2Vec2
+     */
     private static b2Vec2 factory() {
         return new b2Vec2(-1L, true);
     }
-    
+
+    /**
+     * Reserve memory for the new object {@code b2Vec2}.
+     *
+     * @param alloc Custom memory manager
+     * @return b2Vec2
+     */
     public static b2Vec2 alloc(AllocFunc alloc) {
         return new b2Vec2(alloc.alloc(ALIGNOF, SIZEOF, 1));
     }
-    
+
+    /**
+     * Reserve memory for the new object {@code b2Vec2}.
+     *
+     * @return b2Vec2
+     */
     public static b2Vec2 malloc() {
         return new b2Vec2(nmalloc(SIZEOF));
     }
 
+    /**
+     * Reserve an amount n of memory for the object {@code b2Vec2}.
+     *
+     * @param capacity Number of elements
+     * @return Buffer
+     */
     public static Buffer malloc(int capacity) {
         return new Buffer(nmalloc(capacity * SIZEOF), capacity);
     }
-    
+
+    /**
+     * Reserve an amount n of memory for the object {@code b2Vec2}.
+     *
+     * @param capacity Number of elements
+     * @param alloc Custom memory manager
+     * @return Buffer
+     */
+    public static Buffer malloc(int capacity, AllocFunc alloc) {
+        return new Buffer(alloc.alloc(ALIGNOF, SIZEOF, capacity), capacity);
+    }
     
     // -----------------------------------
-    
+
     public static float nx(long address) { return memGetFloat(address + X); }
     public static float ny(long address) { return memGetFloat(address + Y); }
-    
+
     public static void nx(long address, float value) { memPutFloat(address + X, value); }
     public static void ny(long address, float value) { memPutFloat(address + Y, value); }
-    
+
     // -----------------------------------
     
+    /** An array of {@code b2Vec2} structs. */
     public static class Buffer extends StructBuffer<b2Vec2, Buffer> implements JNINative {
-        
-         private static final b2Vec2 ELEMENT_FACTORY = b2Vec2.factory();
+        /** An element that provides information about the structure. */
+        private static final b2Vec2 ELEMENT_FACTORY = b2Vec2.factory();
 
+        /**
+         * Create a new buffer.
+         *
+         * @param address long
+         * @param remaining int
+         */
         public Buffer(long address, int remaining) {
             super(address, remaining);
         }
 
+        /**
+         * Create a new buffer.
+         *
+         * @param address long
+         * @param mark int
+         * @param position int
+         * @param limit int
+         * @param capacity int
+         */
         public Buffer(long address, int mark, int position, int limit, int capacity) {
             super(address, mark, position, limit, capacity);
         }
-        
+
+        /*(non-Jabadoc)
+         */
         @Override
         protected b2Vec2 getElementFactory() {
             return ELEMENT_FACTORY;
         }
 
+        /*(non-Jabadoc)
+         */
         @Override
         protected Buffer self() {
             return this;
         }
 
+        /*(non-Jabadoc)
+         */
         @Override
         protected Buffer create(long address, int mark, int position, int limit, int capacity) {
             return new Buffer(address, mark, position, limit, capacity);
