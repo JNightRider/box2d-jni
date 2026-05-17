306b1b4320f65d90d49c1fb21355c4e7246bfd95
wil <nightrid34@gmail.com>
2026-05-17 17:39:15 -0600

    feat: add b2Rot

--- modules/libraries/core/src/test/java/org/box2d/jni/unit/b2CosSinTest.java
+++ modules/libraries/core/src/test/java/org/box2d/jni/unit/b2CosSinTest.java
@@ -19,7 +19,7 @@ import org.junit.Test;
  */
 public class b2CosSinTest {
     
-        /**
+    /**
      * Initialize all tests.
      */
     @Test
--- modules/libraries/core/src/test/java/org/box2d/jni/unit/b2RotTest.java
+++ modules/libraries/core/src/test/java/org/box2d/jni/unit/b2RotTest.java
@@ -1,13 +1,98 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Copyright Night Rider. All rights reserved.
+ * https://github.com/JNightRider/box2d-jni/blob/master/LICENSE
  */
 package org.box2d.jni.unit;
 
+import org.box2d.jni.b2Rot;
+import static org.box2d.jni.system.Pointer.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
+ * A class to manage the unit tests of the {@link b2Rot} class.
  *
  * @author wil
+ * @version 1.0.0
+ * @since 1.0.0
  */
 public class b2RotTest {
     
+    /**
+     * Initialize all tests.
+     */
+    @Test
+    public void __constructor() {
+        struct();
+        buffer();
+    }
+
+    /**
+     * Test the properties of the structure.
+     */
+    private void struct() {
+        try (b2Rot ptr = b2Rot.malloc()) {
+            Assert.assertNotEquals(NULL, ptr.address());
+
+            ptr.c(2);
+            ptr.s(4);
+
+            Assert.assertEquals(2, ptr.c(), 0);
+            Assert.assertEquals(4, ptr.s(), 0);
+        }
+    }
+
+    /**
+     * Buffer test for {@code Buffer} structures
+     */
+    private void buffer() {
+        b2Rot.Buffer ptr = b2Rot.malloc(5);
+        Assert.assertNotEquals(NULL, ptr.address());
+        Assert.assertEquals(5, ptr.remaining());
+
+        {
+            try (b2Rot obj = b2Rot.malloc()) {
+                obj.c(2).s(3);
+                ptr.put(obj);
+            }
+        }
+        {
+            try (b2Rot obj = b2Rot.malloc()) {
+                obj.c(2).s(3);
+                ptr.put(obj);
+            }
+        }
+        {
+            try (b2Rot obj = b2Rot.malloc()) {
+                obj.c(8.0004f).s(4.5f);
+                ptr.put(obj);
+            }
+        }
+
+        ptr.flip();
+
+        Assert.assertEquals(5, ptr.capacity());
+        Assert.assertEquals(3, ptr.remaining());
+
+        {
+            try (b2Rot obj = ptr.get(0)) {
+                Assert.assertEquals(2f, obj.c(), 0.0f);
+                Assert.assertEquals(3f, obj.s(), 0.0f);
+            }
+        }
+        {
+            try (b2Rot obj = ptr.get(1)) {
+                Assert.assertEquals(2f, obj.c(), 0.0f);
+                Assert.assertEquals(3f, obj.s(), 0.0f);
+            }
+        }
+        {
+            try (b2Rot obj = ptr.get(2)) {
+                Assert.assertEquals(8.0004f, obj.c(), 0.0f);
+                Assert.assertEquals(4.5f, obj.s(), 0.0f);
+            }
+        }
+        ptr.free();
+    }
 }
