13a557b5892b10b4310580daf7d2d118f4564e24
wil <nightrid34@gmail.com>
2026-05-17 19:31:59 -0600

    feat: add b2Mat22

---
+++ modules/libraries/core/src/main/java/org/box2d/jni/b2Mat22.java
@@ -0,0 +1,236 @@
+/*
+ * Copyright Night Rider. All rights reserved.
+ * https://opensource.org/license/bsd-3-clause
+ */
+package org.box2d.jni;
+
+import org.box2d.jni.system.*;
+
+import static org.box2d.jni.libc.LibCStdlib.*;
+import static org.box2d.jni.libc.LibCString.*;
+
+/**
+ * <pre><code>
+ * typedef struct b2Mat22
+ * {
+ * 	/// columns
+ * 	b2Vec2 cx, cy;
+ * } b2Mat22;
+ * </code></pre>
+ *
+ * @author wil
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+public class b2Mat22 extends Struct<b2Mat22> {
+
+    /** The struct size in bytes. */
+    public static final int SIZEOF;
+
+    /** The struct alignment in bytes. */
+    public static final int ALIGNOF;
+
+    /** The struct member offsets. */
+    private static final int
+            CX,
+            CY;
+
+    static {
+        Layout layout = __struct(
+                __member(b2Vec2.SIZEOF, b2Vec2.ALIGNOF),
+                __member(b2Vec2.SIZEOF, b2Vec2.ALIGNOF)
+        );
+
+        CX = layout.offsetof(0);
+        CY = layout.offsetof(1);
+
+        SIZEOF = layout.getSize();
+        ALIGNOF = layout.getAlignment();
+    }
+    
+    /**
+     * Generates a reference to the given pointer.
+     *
+     * @param ptr A reference pointer.
+     */
+    public b2Mat22(Pointer ptr) {
+        super(ptr);
+    }
+
+    /**
+     * Create a new pointer to the object using its memory address.
+     *
+     * @param address A virtual memory address
+     */
+    public b2Mat22(long address) {
+        super(address);
+    }
+
+    /**
+     * This structure is for internal buffer use.
+     *
+     * @param address A virtual memory address
+     * @param factor boolean
+     *
+     * @see Struct#Struct(long, boolean)
+     */
+    protected b2Mat22(long address, boolean factor) {
+        super(address, factor);
+    }
+
+    /** @return Returns the property {@code cx} */
+    public b2Vec2 cx() { return ncx(address()); }
+    /** @return Returns the property {@code cy} */
+    public b2Vec2 cy() { return ncy(address()); }
+    
+    /**
+     * Set the value of property {@code cx}
+     *
+     * @param value The value
+     * @return b2Mat22
+     */
+    public b2Mat22 cx(b2Vec2 value) { ncx(address(), value); return this; }
+
+    /**
+     * Set the value of property {@code cy}
+     *
+     * @param value The value
+     * @return b2Mat22
+     */
+    public b2Mat22 cy(b2Vec2 value) { ncy(address(), value); return this; }
+    
+    /*(non-Javadoc)
+     */
+    @Override
+    public int sizeof() { return SIZEOF; }
+ 
+    /*(non-Javadoc)
+     */
+    @Override
+    protected b2Mat22 create(long address, Pointer ptr) {
+        return ptr == null ? new b2Mat22(address) : new b2Mat22(ptr);
+    }
+    
+    // -----------------------------------
+
+    /**
+     * Internal use of the buffer.
+     * @return b2Mat22
+     */
+    private static b2Mat22 factory() {
+        return new b2Mat22(-1L, true);
+    }
+    
+    /**
+     * Create a reference to a pointer to access its properties.
+     *
+     * @param ptr A reference pointer.
+     * @return b2Mat22
+     */
+    public static b2Mat22 createSafe(Pointer ptr) {
+        if (ptr == null) {
+            return null;
+        }
+        return new b2Mat22(ptr);
+    }
+
+    /**
+     * Reserve memory for the new object {@code b2Mat22}.
+     *
+     * @param alloc Custom memory manager
+     * @return b2Mat22
+     */
+    public static b2Mat22 alloc(AllocFunc alloc) {
+        return new b2Mat22(alloc.alloc(ALIGNOF, SIZEOF, 1));
+    }
+
+    /**
+     * Reserve memory for the new object {@code b2Mat22}.
+     *
+     * @return b2Mat22
+     */
+    public static b2Mat22 malloc() {
+        return new b2Mat22(nmalloc(SIZEOF));
+    }
+
+    /**
+     * Reserve an amount n of memory for the object {@code b2Mat22}.
+     *
+     * @param capacity Number of elements
+     * @return Buffer
+     */
+    public static Buffer malloc(int capacity) {
+        return new Buffer(nmalloc(capacity * SIZEOF), capacity);
+    }
+
+    /**
+     * Reserve an amount n of memory for the object {@code b2Mat22}.
+     *
+     * @param capacity Number of elements
+     * @param alloc Custom memory manager
+     * @return Buffer
+     */
+    public static Buffer malloc(int capacity, AllocFunc alloc) {
+        return new Buffer(alloc.alloc(ALIGNOF, SIZEOF, capacity), capacity);
+    }
+    
+    // -----------------------------------
+
+    public static b2Vec2 ncx(long address) { return b2Vec2.createSafe(() -> address + CX); }
+    public static b2Vec2 ncy(long address) { return b2Vec2.createSafe(() -> address + CY); }
+
+    public static void ncx(long address, b2Vec2 value) { nmemcpy(address + CX, value.address(), b2Vec2.SIZEOF); }
+    public static void ncy(long address, b2Vec2 value) { nmemcpy(address + CY, value.address(), b2Vec2.SIZEOF); }
+
+    // -----------------------------------
+    
+    /** An array of {@code b2Mat22} structs. */
+    public static class Buffer extends StructBuffer<b2Mat22, Buffer> implements JNINative {
+        /** An element that provides information about the structure. */
+        private static final b2Mat22 ELEMENT_FACTORY = b2Mat22.factory();
+
+        /**
+         * Create a new buffer.
+         *
+         * @param address long
+         * @param remaining int
+         */
+        public Buffer(long address, int remaining) {
+            super(address, remaining);
+        }
+
+        /**
+         * Create a new buffer.
+         *
+         * @param address long
+         * @param mark int
+         * @param position int
+         * @param limit int
+         * @param capacity int
+         */
+        public Buffer(long address, int mark, int position, int limit, int capacity) {
+            super(address, mark, position, limit, capacity);
+        }
+
+        /*(non-Jabadoc)
+         */
+        @Override
+        protected b2Mat22 getElementFactory() {
+            return ELEMENT_FACTORY;
+        }
+
+        /*(non-Jabadoc)
+         */
+        @Override
+        protected Buffer self() {
+            return this;
+        }
+
+        /*(non-Jabadoc)
+         */
+        @Override
+        protected Buffer create(long address, int mark, int position, int limit, int capacity) {
+            return new Buffer(address, mark, position, limit, capacity);
+        }        
+    }
+}
---
+++ modules/libraries/core/src/test/java/org/box2d/jni/unit/b2Mat22Test.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright Night Rider. All rights reserved.
+ * https://github.com/JNightRider/box2d-jni/blob/master/LICENSE
+ */
+package org.box2d.jni.unit;
+
+import org.box2d.jni.b2Mat22;
+import org.box2d.jni.b2Vec2;
+import org.box2d.jni.system.Destroyer;
+import static org.box2d.jni.system.Pointer.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * A class to manage the unit tests of the {@link b2Mat22} class.
+ *
+ * @author wil
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class b2Mat22Test {
+
+    /**
+     * Initialize all tests.
+     */
+    @Test
+    public void __constructor() {
+        struct();
+        buffer();
+    }
+
+    /**
+     * Test the properties of the structure.
+     */
+    private void struct() {
+        try (b2Mat22 ptr = b2Mat22.malloc()) {
+            Assert.assertNotEquals(NULL, ptr.address());
+            // set
+            {
+                try (b2Vec2 a = b2Vec2.malloc()) {
+                    a.x(5.6f).y(-1.4f);
+                    ptr.cx(a);
+                }
+
+                try (b2Vec2 b = b2Vec2.malloc()) {
+                    b.x(0.5f).y(1.1f);
+                    ptr.cy(b);
+                }
+            }
+            //get
+            {
+                try (b2Vec2 a = ptr.cx()) {
+                    Assert.assertEquals(5.6f, a.x(), 0.0f);
+                    Assert.assertEquals(-1.4f, a.y(), 0.0f);
+                }
+                try (b2Vec2 b = ptr.cy()) {
+                    Assert.assertEquals(0.5f, b.x(), 0.0f);
+                    Assert.assertEquals(1.1f, b.y(), 0.0f);
+                }
+            }            
+        }
+    }
+    
+    /**
+     * Buffer test for {@code Buffer} structures
+     */
+    private void buffer() {
+        b2Mat22.Buffer ptr = b2Mat22.malloc(5);
+        Assert.assertNotEquals(NULL, ptr.address());
+        Assert.assertEquals(5, ptr.remaining());
+
+        try (Destroyer.Default destroyer = new Destroyer.Default()) {
+            // 1
+            ptr.put(
+                destroyer.create(() -> b2Mat22.malloc()
+                    .cx( /* b2Vec2 */
+                        destroyer.create(() -> b2Vec2.malloc()
+                            .x(1)
+                            .y(2)
+                        )
+                    )
+                    .cy( /* b2Rot */
+                        destroyer.create(() -> b2Vec2.malloc()
+                            .x(0.3f)
+                            .y(1.5f)
+                        )
+                    )
+                ));
+
+            // 2
+            ptr.put(
+                destroyer.create(() -> b2Mat22.malloc()
+                    .cx( /* b2Vec2 */
+                        destroyer.create(() -> b2Vec2.malloc()
+                            .x(5.5f)
+                            .y(2.5f)
+                        )
+                    )
+                    .cy( /* b2Rot */
+                        destroyer.create(() -> b2Vec2.malloc()
+                            .x(3.3f)
+                            .y(10.5f)
+                        )
+                    )
+                ));
+        }
+
+        ptr.flip();
+
+        Assert.assertEquals(5, ptr.capacity());
+        Assert.assertEquals(2, ptr.remaining());
+
+        {
+            try (b2Mat22 obj = ptr.get(0)) {
+                try (b2Vec2 o = obj.cx()) {
+                    Assert.assertEquals(1, o.x(), 0.0f);
+                    Assert.assertEquals(2, o.y(), 0.0f);
+                }
+                try (b2Vec2 o = obj.cy()) {
+                    Assert.assertEquals(0.3f, o.x(), 0.0f);
+                    Assert.assertEquals(1.5f, o.y(), 0.0f);
+                }
+            }
+            
+        }
+        {
+            try (b2Mat22 obj = ptr.get(1)) {
+                try (b2Vec2 o = obj.cx()) {
+                    Assert.assertEquals(5.5f, o.x(), 0.0f);
+                    Assert.assertEquals(2.5f, o.y(), 0.0f);
+                }
+                try (b2Vec2 o = obj.cy()) {
+                    Assert.assertEquals(3.3f, o.x(), 0.0f);
+                    Assert.assertEquals(10.5f, o.y(), 0.0f);
+                }
+            }
+            
+        }
+        ptr.free();
+    }
+}
