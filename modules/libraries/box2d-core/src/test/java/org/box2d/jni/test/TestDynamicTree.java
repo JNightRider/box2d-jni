548480aa6155da35681ec61638b5bed9257ce9f9
wil <nightrid34@gmail.com>
2026-07-26 16:31:05 -0600

    feat: add support 'test_dynamic_tree'

--- modules/libraries/box2d-core/src/test/java/org/box2d/jni/test/TestDynamicTree.java
+++ modules/libraries/box2d-core/src/test/java/org/box2d/jni/test/TestDynamicTree.java
@@ -285,7 +285,7 @@ public class TestDynamicTree {
     private static final b2TreeQueryCallbackFcnI QueryCollectCallback = (proxyId, userData, context) -> 
     {
         IntBuffer out = memIntBuffer(context, 16);
-	    out.put(proxyId, 1);
+        out.put(proxyId, 1);
         return true; // continue the query
     };
 
