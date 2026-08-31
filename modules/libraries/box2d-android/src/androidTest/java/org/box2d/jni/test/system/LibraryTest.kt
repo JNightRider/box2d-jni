ea4a5c86aa9870c94ded2b8c95379997adf08c92
wil <nightrid34@gmail.com>
2026-08-30 22:45:31 -0600

    feat: add test 'LibraryTest'

---
+++ modules/libraries/box2d-android/src/androidTest/java/org/box2d/jni/test/system/LibraryTest.kt
@@ -0,0 +1,65 @@
+/*
+BSD 3-Clause License
+
+Copyright (c) 2026, Night Rider
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.box2d.jni.test.system
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.box2d.jni.system.Library
+import org.box2d.jni.test.util.LibraryUtils
+
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * A class to manage the unit tests of the {@link Library} class.
+ *
+ * @author wil
+ * @version 1.0.0
+ * @since 1.3.0
+ */
+@RunWith(AndroidJUnit4::class)
+class LibraryTest {
+
+    /**
+     * Initialize all tests.
+     */
+    @Before fun setUp() {
+        LibraryUtils.setupLibrary()
+    }
+
+    /**
+     * Run all tests.
+     */
+    @Test fun runTest() {
+        Library.initialize()
+    }
+}
