e7aac30a007aee0ab864905fcd585d1d156dae3a
wil <nightrid34@gmail.com>
2026-09-11 16:03:44 -0600

    fix: create xcframework

--- buildSrc/src/main/java/org/box2d/jni/ios/IOS.java
+++ buildSrc/src/main/java/org/box2d/jni/ios/IOS.java
@@ -86,19 +86,38 @@ public class IOS implements Plugin<Project> {
         TaskProvider<Libtool> libtoolIosDevice = tasks.register("libtoolIosDeviceARM64", Libtool.class, Device.device_arm64);
         TaskProvider<Libtool> libtoolIosSimulatorArm64 = tasks.register("libtoolIosSimulator_ARM64", Libtool.class, Device.simulator_arm64);
         TaskProvider<Libtool> libtoolIosSimulatorX86_64 = tasks.register("libtoolIosSimulator_x86_64", Libtool.class, Device.simulator_x86_64);
-        
+
         Map<BuildType, Map<Flavor, TaskProvider<Xcframework>>> xcfMap = new HashMap<>();
         for (BuildType type : BuildType.values()) {
             Map<Flavor, TaskProvider<Xcframework>> map = new HashMap<>();
-            
+
             for (Flavor flavor : Flavor.values()) {
                 TaskProvider<Xcframework> taskXcframework = tasks.register("XcframeworkIos" + type.getName() + flavor.getName(), Xcframework.class, type, flavor);
+                taskXcframework.configure((task) -> {
+                    IOSProperties iosp = target.getExtensions().getByType(IOSProperties.class);
+                    Device[] devices = Device.parseValues(
+                            iosp.getDevices().get()
+                    );
+
+                    for (Device device : devices) {
+                        switch (device) {
+                            case device_arm64 -> 
+                                task.dependsOn(libtoolIosDevice);
+                            case simulator_arm64 -> 
+                                task.dependsOn(libtoolIosSimulatorArm64);
+                            case simulator_x86_64 -> 
+                                task.dependsOn(libtoolIosSimulatorX86_64);
+                            default ->
+                                throw new AssertionError();
+                        }
+                    }
+                });
                 map.put(flavor, taskXcframework);
             }
-            
+
             xcfMap.put(type, map);
         }
-        
+
         
         // --- [ configure ] ---
         configureIosDevice.configure((task) -> {
@@ -135,41 +154,12 @@ public class IOS implements Plugin<Project> {
         });
         
         build.configure((task) -> {
-            IOSProperties iosp = target.getExtensions().getByType(IOSProperties.class);
-            Device[] devices = Device.parseValues(
-                iosp.getDevices().get()
-            );
-
-            List<TaskProvider<? extends DefaultTask>> listDepends = new ArrayList<>();
-            for (Device device : devices) {
-                switch (device) {
-                    case device_arm64 -> {
-                        listDepends.add(libtoolIosDevice);
-                        task.dependsOn(libtoolIosDevice);
-                    }
-                    case simulator_arm64 -> {
-                        listDepends.add(libtoolIosSimulatorArm64);
-                        task.dependsOn(libtoolIosSimulatorArm64);
-                    }
-                    case simulator_x86_64 -> {
-                        listDepends.add(libtoolIosSimulatorX86_64);
-                        task.dependsOn(libtoolIosSimulatorX86_64);
-                    }
-                    default ->
-                        throw new AssertionError();
-                }
-            }
-            
+            IOSProperties iosp = target.getExtensions().getByType(IOSProperties.class);            
             iosp.getBuildTypes().all((type) -> {
                 Map<Flavor, TaskProvider<Xcframework>> entry = xcfMap.get(type.getBuildType().get());
                 
                 iosp.getProductFlavors().all((flavor) -> {
                     TaskProvider<Xcframework> deps = entry.get(flavor.getFlavor().get());
-                    deps.configure((t) -> {
-                        for (TaskProvider<? extends DefaultTask> provider : listDepends) {
-                            t.dependsOn(provider);
-                        }
-                    });
                     task.dependsOn(deps);
                 });
             });
